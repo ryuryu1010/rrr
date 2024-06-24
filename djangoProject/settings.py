d0766bde24069efd592e96d4e8c2499d25993769
ryuryu1010 <211015@std.hi-joho.ac.jp>
2024-06-24 15:01:01 +0900

    バグり中

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'myapp.apps.MyappConfig',
 ]
 
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- myapp/views.py
+++ myapp/views.py
@@ -1,29 +1,29 @@
-# myapp/views.py
-
+# views.py
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
-from django.conf import settings
-import sys
-import os
+from image_processing import extract_text_from_image  # インポート文
+from django.core.exceptions import SuspiciousFileOperation
 
-# Pythonプロジェクトのパスを追加
-sys.path.append(os.path.join(settings.BASE_DIR, '..', 'image_processing'))
-from image_processing import image_processing
-
+# 画像アップロード機能の関数
 def upload_image(request):
     if request.method == 'POST' and request.FILES['image']:
         image = request.FILES['image']
         fs = FileSystemStorage()
         filename = fs.save(image.name, image)
         image_url = fs.url(filename)
-        return redirect('result') + f'?image_url={image_url}'
+        return redirect('result') + f'?image_url={filename}'
     return render(request, 'upload_image.html')
 
+# 結果表示の関数
 def result(request):
     image_url = request.GET.get('image_url')
     if image_url:
         fs = FileSystemStorage()
-        image_path = fs.path(image_url)
-        extracted_text = image_processing.extract_text_from_image(image_path)
-        return render(request, 'result.html', {'extracted_text': extracted_text, 'image_url': image_url})
+        try:
+            image_path = fs.path(image_url)
+            extracted_text = extract_text_from_image(image_path)
+            return render(request, 'result.html', {'extracted_text': extracted_text, 'image_url': fs.url(image_url)})
+        except SuspiciousFileOperation:
+            # 不審なファイル操作の例外処理
+            return redirect('upload_image')
     return redirect('upload_image')
